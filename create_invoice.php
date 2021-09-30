24e9eb9b845b71217b71a827d318c881bd1da643
salludin <salludingozali@gmail.com>
2021-09-30 09:07:04 +0700

    integrasi sendpulse

--- create_invoice.php
+++ create_invoice.php
@@ -15,21 +15,21 @@ use Xendit\Xendit;
 
 require 'vendor/autoload.php';
 require('bootloader.php');
-	$get_profile = $db->query(sprintf("SELECT * FROM users WHERE user_id = %s", secure($_GET['user_id']))) or _error("SQL_ERROR_THROWEN");
-	if($get_profile->num_rows == 0) {
-		_error(404);
-	}
-	$profile = $get_profile->fetch_assoc();
-	$user_email = $profile['user_email'];
-	$user_phone = $profile['user_phone'];
-	$fullname = $profile['user_firstname'] . ' ' . $profile['user_lastname'];
+    $get_profile = $db->query(sprintf("SELECT * FROM users WHERE user_id = %s", secure($_GET['user_id']))) or _error("SQL_ERROR_THROWEN");
+    if($get_profile->num_rows == 0) {
+        _error(404);
+    }
+    $profile = $get_profile->fetch_assoc();
+    $user_email = $profile['user_email'];
+    $user_phone = $profile['user_phone'];
+    $fullname = $profile['user_firstname'] . ' ' . $profile['user_lastname'];
     $get_event = $db->query(sprintf("SELECT * FROM events  WHERE event_id = %s", secure($_GET['event_id']) )) or _error("SQL_ERROR_THROWEN");
-	if($get_event->num_rows == 0) {
-		_error(404);
-	}
-	$event = $get_event->fetch_assoc();
-	$event_name = $event['event_title'];
-	$event_amount = $event['event_amount'];
+    if($get_event->num_rows == 0) {
+        _error(404);
+    }
+    $event = $get_event->fetch_assoc();
+    $event_name = $event['event_title'];
+    $event_amount = $event['event_amount'];
 Xendit::setApiKey('xnd_development_yiVQcbbYvEgmbUE9reiJBmXbdm2r0SzjsE16lk3IykQrHbKw1JnToeNbzUwrT6i');
 
 $params = ['external_id' => $_GET['event_id'] . ' - ' . $_GET['user_id']. ' - ' . $event_name,
@@ -48,6 +48,9 @@ $createInvoice = \Xendit\Invoice::create($params);
 
 $id = $createInvoice['id'];
 $url = $createInvoice['invoice_url'];
+$end_date = $createInvoice['expiry_date'];
+$Name = $createInvoice['given_names'];
+$item_name = $createInvoice['name'];
 $subject = 'Informasi Pembayaran Event ' . $event_name . '';
 
 $body = get_email_template("notification_payment_email", $subject, ["first_name" => $profile['first_name'], "last_name" => $profile['last_name'], "email_verification_code" => $url]);
---
+++ sendpulse/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0d4418948d2983ddaaf0e183dde09f6d0edc5101',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0d4418948d2983ddaaf0e183dde09f6d0edc5101',
+            'dev_requirement' => false,
+        ),
+        'sendpulse/rest-api' => array(
+            'pretty_version' => '1.0.26',
+            'version' => '1.0.26.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sendpulse/rest-api',
+            'aliases' => array(),
+            'reference' => 'e20b957c9b3e88563f449622c1f963f81e47c2d8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
