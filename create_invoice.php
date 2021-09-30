51792460b9afdcb3bdf9a4e6937a97e548cbc082
salludin <salludingozali@gmail.com>
2021-09-30 09:43:15 +0700

    fixing bug xendit & integrasi sendpulse

--- create_invoice.php
+++ create_invoice.php
@@ -39,6 +39,15 @@ $params = ['external_id' => $_GET['event_id'] . ' - ' . $_GET['user_id']. ' - ' 
         'email' => $user_email,
         'mobile_number' => $user_phone
     ],
+    'items' => [
+        'name' => $event_name,
+        'quantity' => 1,
+        'price' => $event_amount
+    ],
+    'fees' => [
+        'type' => 'ADMIN',
+        'value' => 3650
+    ],
     'description' => 'Pendaftaran Event ' . $event_name . '',
     'amount' => $event_amount
 ];
@@ -47,12 +56,54 @@ $createInvoice = \Xendit\Invoice::create($params);
 
 
 $id = $createInvoice['id'];
+$external_id = $createInvoice['external_id'];
 $url = $createInvoice['invoice_url'];
 $end_date = $createInvoice['expiry_date'];
 $Name = $createInvoice['given_names'];
 $item_name = $createInvoice['name'];
 $subject = 'Informasi Pembayaran Event ' . $event_name . '';
 
-$body = get_email_template("notification_payment_email", $subject, ["first_name" => $profile['first_name'], "last_name" => $profile['last_name'], "email_verification_code" => $url]);
+//$body = get_email_template("notification_payment_email", $subject, ["first_name" => $profile['first_name'], "last_name" => $profile['last_name'], "email_verification_code" => $url]);
                 /* send email */
-$send_email = _email($user_email, $subject, $body['html'], $body['html']);
+//$send_email = _email($user_email, $subject, $body['html'], $body['html']);
+
+// integrasi sendpulse //
+
+require 'sendpulse/vendor/autoload.php';
+
+use Sendpulse\RestApi\ApiClient;
+use Sendpulse\RestApi\Storage\FileStorage;
+
+define('API_USER_ID', 'ff9c835aa7da718ad006ee4bc029ef92');
+define('API_SECRET', 'bf6cc366d6c2e01c837ff517c8a19cfb');
+define('PATH_TO_ATTACH_FILE', __FILE__);
+
+$SPApiClient = new ApiClient(API_USER_ID, API_SECRET, new FileStorage());
+
+$email = array(
+    'subject' => $subject,
+    'template' => array(
+        'id' => '662224',
+    ),
+    'variables' => array(
+        'Name' => $Name,
+        'end_date' => $end_date,
+        'order_id' => $external_id,
+        'item.name' => $item_name,
+        'item.price' => $event_amount,
+
+    ),
+    'from' => array(
+        'name' => 'Teachin.id',
+        'email' => 'admin@teachin.id',
+    ),
+    'to' => array(
+        array(
+            'email' => $user_email,
+        ),
+    ),
+);
+var_dump($SPApiClient->smtpSendMail($email));
+
+
+header('Location: '.$url);
