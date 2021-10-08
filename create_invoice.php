55846fae257540afb9fbd001250a3f7cc0c05fa9
salludin <salludingozali@gmail.com>
2021-10-08 08:52:34 +0700

    add function convert number to phone format

--- create_invoice.php
+++ create_invoice.php
@@ -27,8 +27,32 @@ require('bootloader.php');
     $fullname = $profile['user_firstname'] . ' ' . $profile['user_lastname']; 
     }else{
     $user_email = $_POST['email'];
-    $user_phone = $_POST['phone'];
     $fullname = $_POST['fullname'];
+    $nohp = $_POST['phone'];
+    function hp($nohp) {
+     // kadang ada penulisan no hp 0811 239 345
+     $nohp = str_replace(" ","",$nohp);
+     // kadang ada penulisan no hp (0274) 778787
+     $nohp = str_replace("(","",$nohp);
+     // kadang ada penulisan no hp (0274) 778787
+     $nohp = str_replace(")","",$nohp);
+     // kadang ada penulisan no hp 0811.239.345
+     $nohp = str_replace(".","",$nohp);
+ 
+     // cek apakah no hp mengandung karakter + dan 0-9
+     if(!preg_match('/[^+0-9]/',trim($nohp))){
+         // cek apakah no hp karakter 1-3 adalah +62
+         if(substr(trim($nohp), 0, 3)=='+62'){
+             $hp = trim($nohp);
+         }
+         // cek apakah no hp karakter 1 adalah 0
+         elseif(substr(trim($nohp), 0, 1)=='0'){
+             $hp = '+62'.substr(trim($nohp), 1);
+         }
+     }
+     print $hp;
+    }
+    $user_phone = hp($nohp);
     }
 
     $get_event = $db->query(sprintf("SELECT * FROM events  WHERE event_id = %s", secure($_GET['event_id']) )) or _error("SQL_ERROR_THROWEN");
